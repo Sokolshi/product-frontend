ed44e0c13100fc07363a7a57c4339de57026dabe
Sokolshi <sokoll050414@gmail.com>
2026-06-22 10:29:04 +0800

    remove image from add product

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export function getProducts() {
-  return axios.get('http://192.168.1.110:3000/products')
+  return axios.get('https://product-backend-2qik.onrender.com/products')
 }
--- src/views/AddProductView.vue
+++ src/views/AddProductView.vue
@@ -1,17 +1,13 @@
 <template>
-  <div>
-    <h1>Бараа нэмэх</h1>
+  <div class="container">
+    <h2>Бараа нэмэх</h2>
 
     <form @submit.prevent="addProduct">
-      <input v-model="Нэр" placeholder="Нэр" />
+      <input v-model="Нэр" type="text" placeholder="Барааны нэр" />
 
-      <br /><br />
+      <input v-model="Үнэ" type="number" placeholder="Үнэ" />
 
-      <input v-model="Үнэ" type="number" placeholder="Үнэ" step="100" min="0" />
-
-      <br /><br />
-
-      <button type="submit">Нэмэх</button>
+      <button type="submit">Add Product</button>
     </form>
   </div>
 </template>
@@ -21,17 +17,42 @@ import { ref } from 'vue'
 import axios from 'axios'
 import { useRouter } from 'vue-router'
 
-const Нэр = ref('')
-const Үнэ = ref('')
-
 const router = useRouter()
 
-async function addProduct() {
-  await axios.post('https://product-backend-2qik.onrender.com/products', {
-    name: Нэр.value,
-    price: Number(Үнэ.value),
-  })
+const name = ref('')
+const price = ref('')
 
-  router.push('/products')
+const addProduct = async () => {
+  try {
+    await axios.post('https://product-backend-2qik.onrender.com/products', {
+      name: name.value,
+      price: Number(price.value),
+    })
+
+    alert('Product added')
+
+    router.push('/products')
+  } catch (error) {
+    console.log(error)
+
+    alert('Алдаа гарлаа')
+  }
 }
 </script>
+
+<style scoped>
+.container {
+  width: 400px;
+  margin: 40px auto;
+}
+
+input {
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 15px;
+}
+
+button {
+  padding: 10px 20px;
+}
+</style>
